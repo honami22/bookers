a6c493c9702aa12e3a24aba4a7af6702a383346c
EC2 Default User <ec2-user@ip-172-31-2-209.ap-northeast-1.compute.internal>
2023-05-12 15:57:29 +0000

    [mod0] 課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,13 +1,14 @@
 class BooksController < ApplicationController
   def new
     @book = Book.new
+    @books = Book.all
   end
 
   def top
     @book = Book.new
   end
 
-   def create
+  def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
     book = Book.new(book_params)
     # 3. データをデータベースに保存するためのsaveメソッド実行
@@ -15,12 +16,6 @@ class BooksController < ApplicationController
     # 4. トップ画面へリダイレクト
     # redirect_to '/top'
     redirect_to book_path(book.id)
-
-   end
-
-
-  def index
-      @books = Book.all
 
   end
 
@@ -36,6 +31,7 @@ class BooksController < ApplicationController
 
   def destroy
     book = Book.find(params[:id])
+    # book = Book.find(params[:new])
     book.destroy
     redirect_to '/books'
   end
