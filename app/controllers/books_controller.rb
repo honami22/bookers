234a6fe4a6bbfbbc4bd13cc3d1db7cfd951d8b8f
EC2 Default User <ec2-user@ip-172-31-2-209.ap-northeast-1.compute.internal>
2023-05-13 14:27:55 +0000

    [modo] 課題２

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -10,12 +10,20 @@ class BooksController < ApplicationController
 
   def create
     # １.&2. データを受け取り新規登録するためのインスタンス作成
-    book = Book.new(book_params)
-    # 3. データをデータベースに保存するためのsaveメソッド実行
-    book.save
-    # 4. トップ画面へリダイレクト
-    # redirect_to '/top'
-    redirect_to book_path(book.id)
+    # book = Book.new(book_params)
+    # # 3. データをデータベースに保存するためのsaveメソッド実行
+    # book.save
+    # # 4. トップ画面へリダイレクト
+    # # redirect_to '/top'
+    # redirect_to book_path(book.id)
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    else
+      @books = Book.all
+      render :new
+
+    end
 
   end
 
