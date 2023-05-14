7f7805ed41dbbd80a4bdacc946937e313c6ff515
EC2 Default User <ec2-user@ip-172-31-2-209.ap-northeast-1.compute.internal>
2023-05-14 13:47:13 +0000

    [modo] 課題2

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -29,11 +29,12 @@ class BooksController < ApplicationController
 
   def show
      @book = Book.find(params[:id])
-
+   
   end
 
   def edit
       @book = Book.find(params[:id])
+      @books = Book.all
 
   end
 
